2fd2c10a8417dca4c186aee7c99ce8c3f2d880a5
Gustne <148748819+Gustne@users.noreply.github.com>
2024-02-20 09:40:22 +0100

    Add project files.

---
+++ CompareToOpgave/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+
+using Models;
+
+Person[] people = new Person[4];
+
+// Tilføj personer til arrayet
+people[0] = new Person { Id = 1, Name = "Alice", Age = 30 };
+people[1] = new Person { Id = 2, Name = "Bob", Age = 15 };
+people[2] = new Person { Id = 3, Name = "Charlie", Age = 35 };
+people[3] = new Person { Id = 4, Name = "Diana", Age = 32 };
+
+
+
+foreach (Person p in people)
+{
+    Console.WriteLine(p);
+}
+
+
+Array.Sort(people);
+
+
+Console.WriteLine(  );
+foreach (Person p in people)
+{
+    Console.WriteLine(p);
+}
---
+++ Models/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace Models
+{
+    public class Person : IComparable<Person>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age {  get; set; }
+
+        public int CompareTo(Person other)
+        {
+            return this.Age.CompareTo(other.Age);
+        }
+
+        public override string ToString()
+        {
+            return $"id: {this.Id}, Name: {this.Name}, Age: {this.Age}";
+        }
+
+    }
+}
